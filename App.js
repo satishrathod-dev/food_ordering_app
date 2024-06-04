fa5cd93147b9bfe9a87024045ace00add0301b4e
Satish Rathod <satishrathod.dev@gmail.com>
2024-06-04 23:33:13 +0530

    episode2

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("h2", {}, "hey there!"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("h1", {}, "Hot module Replacement"),
+    React.createElement("h2", {}, "hello"),
   ])
 );
 
-const heading = React.createElement("h1", {}, "Hello React!");
+// const heading = React.createElement("h1", {}, "Hello React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-
-root.render(heading);
+// root.render(heading);
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Namaste React 🚀
+# Namaste React 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement ->
+- File Watching Algorithm - written in c++
+- caching = Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling = error suggestions
+- HTTPs
+- Tree Shaking = remove unused code
+- Different dev and prod builds
