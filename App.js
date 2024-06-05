6afd1352a8ed1ddbcd59697fc7b2df1c6311eb87
Satish Rathod <satishrathod.dev@gmail.com>
2024-06-05 09:16:44 +0530

    episode3

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
+/*
+// core way of creating react element or obj:
 const parent = React.createElement(
   "div",
   { id: "parent" },
@@ -21,3 +23,43 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
 root.render(parent);
+*/
+
+const headings = (
+  <div>
+    <h1>Hello JSX!</h1>
+    <h2>Hello JSX2!</h2>
+  </div>
+);
+
+/*const HeadingComponent = () => {
+  return <h1>Hello Functional Component!</h1>;
+};*/
+
+//When you are creating functional component use arrow function: convention
+// const Title = () => <h1 className="heading">Hello from Arrow Function!</h1>;
+
+const elem = <span>React Element! </span>;
+const title = <h1 className="heading">{elem}I am JavaScript Variable!</h1>;
+
+/*
+//Normal function (Anonymous)
+const Title = function () {
+  return <h1>Hello From Normal Function</h1>;
+};
+*/
+
+// Component Compostion: Merging two components
+const HeadingComponent = () => (
+  <div id="container">
+    {/* Use {curlyBraces} for JS ==> This will help to get rid of malicius data => From some random api*/}
+    <h2>{console.log("I am JavaScript inside JSX!")}</h2>
+    {/* <Title /> */}
+    {title}
+    <h1 className="heading">Hello Functional Component!</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
